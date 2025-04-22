5ee2f7866ec12e5349d4bbd665c57e2730df17a5
Nimesh Sanka Lakmal <nimeshsanka1209@gmail.com>
2025-04-22 14:19:18 +0530

    Update Demo.java

--- system2/src/Demo.java
+++ system2/src/Demo.java
@@ -1,5 +1,10 @@
+import java.sql.Connection;
+
 public class Demo {
     public static void main(String[] args) {
+        Dbconnector dbc = new Dbconnector();
+        Connection con = dbc.getConnection();
+
         
     }
 }
