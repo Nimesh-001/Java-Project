7adadf41ce3e54d500a8899709ae39600386c2b9
wasantha2000 <www.wasanthakeerthi9@gmail.com>
2025-04-23 01:38:55 +0530

    Update lecmarks form

--- system2/src/LecMarks.java
+++ system2/src/LecMarks.java
@@ -1,2 +1,36 @@
+import javax.swing.*;
+
 public class LecMarks {
+    private JPanel panel1;
+    private JComboBox comboBox1;
+    private JTextField textField1;
+    private JButton ADDButton;
+    private JComboBox comboBox2;
+    private JTextField textField2;
+    private JTextField textField3;
+    private JTextField textField4;
+    private JButton ADDButton1;
+    private JButton ADDButton2;
+    private JButton ADDButton3;
+    private JTextField textField5;
+    private JTextField textField6;
+    private JTextField textField7;
+    private JTextField textField8;
+    private JTextField textField9;
+    private JButton ADDButton4;
+    private JButton ADDButton5;
+    private JButton ADDButton6;
+    private JButton ADDButton7;
+    private JButton ADDButton8;
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("LecMarks");
+        frame.setContentPane(new LecMarks().panel1);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+        frame.setLocationRelativeTo(null);
+        frame.setResizable(false);
+        frame.setSize(1000,500);
+    }
 }
