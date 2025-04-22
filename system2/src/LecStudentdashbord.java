e54f0453d51f7cf1bd6d1f6bc80d409ab91a0f99
wasantha2000 <www.wasanthakeerthi9@gmail.com>
2025-04-22 23:05:46 +0530

    Creare LecStudentdashbord form

--- system2/src/LecStudentdashbord.java
+++ system2/src/LecStudentdashbord.java
@@ -1,2 +1,21 @@
+import javax.swing.*;
+
 public class LecStudentdashbord {
+    private JPanel panel1;
+    private JButton detailsButton;
+    private JButton eligibilityButton;
+    private JButton marksGradeGPAButton;
+    private JButton attendanceButton;
+    private JButton medicalButton;
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("LecStudentdashbord");
+        frame.setContentPane(new LecStudentdashbord().panel1);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+        frame.setLocationRelativeTo(null);
+        frame.setResizable(false);
+        frame.setSize(1000,500);
+    }
 }
