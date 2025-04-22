92f727a618bde9bb58c71f0c5ccb30dbd76095f9
wasantha2000 <www.wasanthakeerthi9@gmail.com>
2025-04-23 00:51:22 +0530

    Update lecCourse

--- system2/src/LecCourses.java
+++ system2/src/LecCourses.java
@@ -2,4 +2,18 @@ import javax.swing.*;
 
 public class LecCourses {
     private JPanel panel1;
+    private JComboBox comboBox1;
+    private JButton ADDButton;
+    private JButton DELETEButton;
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("LecCourses");
+        frame.setContentPane(new LecCourses().panel1);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+        frame.setLocationRelativeTo(null);
+        frame.setResizable(false);
+        frame.setSize(1000,500);
+    }
 }
