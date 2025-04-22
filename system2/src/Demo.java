68e2994dd82dda14e0ab20372a5ca27ac60f517c
Nimesh Sanka Lakmal <nimeshsanka1209@gmail.com>
2025-04-22 14:23:38 +0530

    Update Demo.java

--- system2/src/Demo.java
+++ system2/src/Demo.java
@@ -5,6 +5,10 @@ public class Demo {
         Dbconnector dbc = new Dbconnector();
         Connection con = dbc.getConnection();
 
-        
+        if (con != null) {
+            System.out.println("Connection object is ready to use.");
+        } else {
+            System.out.println("Connection object is null.");
+        }
     }
 }
