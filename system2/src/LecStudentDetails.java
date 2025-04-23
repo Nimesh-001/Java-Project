714327a7ce63afc8d59995dce601145bed8f7503
wasantha2000 <www.wasanthakeerthi9@gmail.com>
2025-04-23 23:05:58 +0530

    Update lecstudentdetails

--- system2/src/LecStudentDetails.java
+++ system2/src/LecStudentDetails.java
@@ -1,2 +1,20 @@
+import javax.swing.*;
+
 public class LecStudentDetails {
+    private JPanel panel1;
+    private JTextField textField1;
+    private JButton viewButton;
+    private JButton clearButton;
+    private JButton backButton;
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("LecStudentDetails");
+        frame.setContentPane(new LecStudentDetails().panel1);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+        frame.setLocationRelativeTo(null);
+        frame.setResizable(false);
+        frame.setSize(1000,500);
+    }
 }
