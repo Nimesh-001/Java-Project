7d7ef61cf159ed02657338baa34ae8f37d2d8a73
wasantha2000 <www.wasanthakeerthi9@gmail.com>
2025-04-24 01:53:43 +0530

    Update LecStudentMarks.java

--- system2/src/LecStudentMarks.java
+++ system2/src/LecStudentMarks.java
@@ -1,2 +1,9 @@
+import javax.swing.*;
+
 public class LecStudentMarks {
+    private JPanel panel1;
+    private JButton backButton;
+    private JTextField textField1;
+    private JComboBox comboBox1;
+    private JButton viewButton;
 }
