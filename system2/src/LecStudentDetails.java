5cac5ce6085317897897e6a3d00ef2beb3f36b5e
wasantha2000 <www.wasanthakeerthi9@gmail.com>
2025-04-24 01:38:58 +0530

    Update LecStudentDetails.java

--- system2/src/LecStudentDetails.java
+++ system2/src/LecStudentDetails.java
@@ -1,4 +1,6 @@
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class LecStudentDetails {
     private JPanel panel1;
@@ -7,14 +9,39 @@ public class LecStudentDetails {
     private JButton clearButton;
     private JButton backButton;
 
-    public static void main(String[] args) {
+    public LecStudentDetails() {
+
         JFrame frame = new JFrame("LecStudentDetails");
-        frame.setContentPane(new LecStudentDetails().panel1);
+        frame.setContentPane(this.panel1);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.pack();
         frame.setVisible(true);
         frame.setLocationRelativeTo(null);
         frame.setResizable(false);
         frame.setSize(1000,500);
+
+        backButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                frame.dispose();
+                new LecStudentdashbord();
+            }
+        });
+        viewButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+        clearButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+    }
+
+    public static void main(String[] args) {
+       new LecStudentDetails();
     }
 }
