1f6e361179ddcd98576deb76e0e765f7f4e24fdc
wasantha2000 <www.wasanthakeerthi9@gmail.com>
2025-04-24 01:23:23 +0530

    Update LecNotice.java

--- system2/src/LecNotice.java
+++ system2/src/LecNotice.java
@@ -1,17 +1,32 @@
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class LecNotice {
     private JPanel panel1;
     private JButton backButton;
 
-    public static void main(String[] args) {
+    public LecNotice() {
+
         JFrame frame = new JFrame("LecNotice");
-        frame.setContentPane(new LecNotice().panel1);
+        frame.setContentPane(this.panel1);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.pack();
         frame.setVisible(true);
         frame.setLocationRelativeTo(null);
         frame.setResizable(false);
         frame.setSize(1000,500);
+
+        backButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                frame.dispose();
+                new Lecturdashbord();
+            }
+        });
+    }
+
+    public static void main(String[] args) {
+        new LecNotice();
     }
 }
