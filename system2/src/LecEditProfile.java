cbd79b615f363acd8b6fa98a7de02f26e82c5a42
wasantha2000 <www.wasanthakeerthi9@gmail.com>
2025-04-24 01:22:50 +0530

    Update LecEditProfile.java

--- system2/src/LecEditProfile.java
+++ system2/src/LecEditProfile.java
@@ -1,4 +1,6 @@
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class LecEditProfile {
     private JPanel panel1;
@@ -8,14 +10,33 @@ public class LecEditProfile {
     private JButton OKButton;
     private JTextField textField2;
 
-    public static void main(String[] args) {
+    public LecEditProfile() {
+
         JFrame frame = new JFrame("LecEditProfile");
-        frame.setContentPane(new LecEditProfile().panel1);
+        frame.setContentPane(this.panel1);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.pack();
         frame.setVisible(true);
         frame.setLocationRelativeTo(null);
         frame.setResizable(false);
         frame.setSize(1000,500);
+
+        backButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                frame.dispose();
+                new Lecturdashbord();
+            }
+        });
+        OKButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+    }
+
+    public static void main(String[] args) {
+        new LecEditProfile();
     }
 }
