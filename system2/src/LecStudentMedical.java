9971a9955f3d5ef06c3e906a21d6f41c58fcc1e9
wasantha2000 <www.wasanthakeerthi9@gmail.com>
2025-04-24 00:05:50 +0530

    Update LecStudentMedical

--- system2/src/LecStudentMedical.java
+++ system2/src/LecStudentMedical.java
@@ -1,2 +1,20 @@
+import javax.swing.*;
+
 public class LecStudentMedical {
+    private JPanel panel1;
+    private JTextField textField1;
+    private JButton viewButton;
+    private JComboBox comboBox1;
+    private JButton backButton;
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("LecStudentMedical");
+        frame.setContentPane(new LecStudentMedical().panel1);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+        frame.setLocationRelativeTo(null);
+        frame.setResizable(false);
+        frame.setSize(1000,500);
+    }
 }
