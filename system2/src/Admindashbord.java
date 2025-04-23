818784a8bc1bc997b78a243f6ae6bfc576c0674c
Nimesh Sanka Lakmal <nimeshsanka1209@gmail.com>
2025-04-23 15:22:29 +0530

    update

--- system2/src/Admindashbord.java
+++ system2/src/Admindashbord.java
@@ -1,4 +1,6 @@
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class Admindashbord {
     private JPanel panel1;
@@ -8,7 +10,6 @@ public class Admindashbord {
     private JButton noticesButton1;
     private JButton coursesButton1;
     private JButton userProfilesButton1;
-    private JButton button1;
 
     public Admindashbord() {
         JFrame frame = new JFrame("Admindashbord");
@@ -20,6 +21,44 @@ public class Admindashbord {
         frame.setLocationRelativeTo(null);
         frame.setSize(1000,500);
         frame.setVisible(true);
+
+
+        userProfilesButton1.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+        coursesButton1.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+        noticesButton1.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+        timetablesButton1.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+        editProfileButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+        logoutButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
     }
 
     public static void main(String[] args) {
