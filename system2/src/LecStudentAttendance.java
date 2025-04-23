7b02da5923023f535356f966295758d7127a98a5
wasantha2000 <www.wasanthakeerthi9@gmail.com>
2025-04-24 02:13:19 +0530

    Update LecStudentAttendance.java

--- system2/src/LecStudentAttendance.java
+++ system2/src/LecStudentAttendance.java
@@ -1,4 +1,6 @@
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class LecStudentAttendance {
     private JPanel panel1;
@@ -11,14 +13,45 @@ public class LecStudentAttendance {
     private JComboBox comboBox1;
     private JComboBox comboBox2;
 
-    public static void main(String[] args) {
+    public LecStudentAttendance() {
+
         JFrame frame = new JFrame("LecStudentAttendance");
-        frame.setContentPane(new LecStudentAttendance().panel1);
+        frame.setContentPane(this.panel1);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.pack();
         frame.setVisible(true);
         frame.setLocationRelativeTo(null);
         frame.setResizable(false);
         frame.setSize(1000,500);
+
+        backButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                frame.dispose();
+                new LecStudentdashbord();
+            }
+        });
+        viewButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+        clearButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+        viewButton1.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+    }
+
+    public static void main(String[] args) {
+        new LecStudentAttendance();
     }
 }
