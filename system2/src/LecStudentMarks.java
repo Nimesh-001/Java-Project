b52fcfbe387cabccf14dee876824f6b11c52d0b4
wasantha2000 <www.wasanthakeerthi9@gmail.com>
2025-04-24 02:13:59 +0530

    Update LecStudentMarks.java

--- system2/src/LecStudentMarks.java
+++ system2/src/LecStudentMarks.java
@@ -1,4 +1,6 @@
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class LecStudentMarks {
     private JPanel panel1;
@@ -6,4 +8,34 @@ public class LecStudentMarks {
     private JTextField textField1;
     private JComboBox comboBox1;
     private JButton viewButton;
+
+    public LecStudentMarks() {
+
+        JFrame frame = new JFrame("LecStudentMarks");
+        frame.setContentPane(this.panel1);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+        frame.setLocationRelativeTo(null);
+        frame.setResizable(false);
+        frame.setSize(1000,500);
+
+        backButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                frame.dispose();
+                new LecStudentdashbord();
+            }
+        });
+        viewButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+    }
+
+    public static void main(String[] args) {
+        new LecStudentMarks();
+    }
 }
