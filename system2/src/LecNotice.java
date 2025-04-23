453c3cc0d9aadf10af3b76c90d1d688c8a0d6dff
wasantha2000 <www.wasanthakeerthi9@gmail.com>
2025-04-24 00:16:32 +0530

    Update LecNotice

--- system2/src/LecNotice.java
+++ system2/src/LecNotice.java
@@ -1,2 +1,17 @@
+import javax.swing.*;
+
 public class LecNotice {
+    private JPanel panel1;
+    private JButton backButton;
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("LecNotice");
+        frame.setContentPane(new LecNotice().panel1);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+        frame.setLocationRelativeTo(null);
+        frame.setResizable(false);
+        frame.setSize(1000,500);
+    }
 }
