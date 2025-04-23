55002bb40de1d0fdc30724abf994ad22cb3ea237
wasantha2000 <www.wasanthakeerthi9@gmail.com>
2025-04-23 23:14:50 +0530

    Update LecstudentEligibility

--- system2/src/LecStudentEligibility.java
+++ system2/src/LecStudentEligibility.java
@@ -1,2 +1,21 @@
+import javax.swing.*;
+
 public class LecStudentEligibility {
+    private JPanel panel1;
+    private JTextField textField1;
+    private JButton viewButton;
+    private JButton clearButton;
+    private JButton backButton;
+    private JTable table1;
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("LecStudentEligibility");
+        frame.setContentPane(new LecStudentEligibility().panel1);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+        frame.setLocationRelativeTo(null);
+        frame.setResizable(false);
+        frame.setSize(1000,500);
+    }
 }
