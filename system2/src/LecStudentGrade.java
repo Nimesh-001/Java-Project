443cceaa57192db0b1da0e6ae9a4de65d9568ee5
wasantha2000 <www.wasanthakeerthi9@gmail.com>
2025-04-23 23:43:15 +0530

    Update LecStudentGrade

--- system2/src/LecStudentGrade.java
+++ system2/src/LecStudentGrade.java
@@ -1,2 +1,19 @@
+import javax.swing.*;
+
 public class LecStudentGrade {
+    private JPanel panel1;
+    private JButton viewResultsButton;
+    private JButton backButton;
+    private JTable table1;
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("LecStudentGrade");
+        frame.setContentPane(new LecStudentGrade().panel1);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+        frame.setLocationRelativeTo(null);
+        frame.setResizable(false);
+        frame.setSize(1000,500);
+    }
 }
