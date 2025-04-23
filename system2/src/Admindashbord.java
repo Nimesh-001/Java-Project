0666c91729888f3953e2370abfb05847709f3646
Nimesh Sanka Lakmal <nimeshsanka1209@gmail.com>
2025-04-24 01:33:05 +0530

    Update Admindashbord.java

--- system2/src/Admindashbord.java
+++ system2/src/Admindashbord.java
@@ -1,6 +1,10 @@
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 public class Admindashbord {
     private JPanel panel1;
@@ -21,6 +25,8 @@ public class Admindashbord {
         frame.setResizable(false);
         frame.setLocationRelativeTo(null);
         frame.setSize(1000,500);
+
+        loadprofileimage();
 
 
 
@@ -64,6 +70,25 @@ public class Admindashbord {
 
             }
         });
+    }
+
+    public void loadprofileimage(){
+        Dbconnector dbc = new Dbconnector();
+        Connection con = dbc.getConnection();
+
+        String sql= "SELECT Profile_Pic_Path FROM USER WHERE Username='AD0001'";
+        try {
+            PreparedStatement pst = con.prepareStatement(sql);
+            ResultSet rs = pst.executeQuery();
+
+            if(rs.next()){
+                String path = rs.getString("Profile_Pic_Path");
+
+            }
+
+        } catch (SQLException e) {
+            JOptionPane.showMessageDialog(null, "error loading image:"+e.getMessage());
+        }
     }
 
     public static void main(String[] args) {
