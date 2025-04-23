7af90e5c35e0b65b4321c88dd70e14955cdb9b6c
wasantha2000 <www.wasanthakeerthi9@gmail.com>
2025-04-24 00:40:08 +0530

    Update LecEditProfile

--- system2/src/LecEditProfile.java
+++ system2/src/LecEditProfile.java
@@ -1,2 +1,21 @@
+import javax.swing.*;
+
 public class LecEditProfile {
+    private JPanel panel1;
+    private JTextField textField1;
+    private JPasswordField passwordField1;
+    private JButton backButton;
+    private JButton OKButton;
+    private JTextField textField2;
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("LecEditProfile");
+        frame.setContentPane(new LecEditProfile().panel1);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+        frame.setLocationRelativeTo(null);
+        frame.setResizable(false);
+        frame.setSize(1000,500);
+    }
 }
