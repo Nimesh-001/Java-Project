3d39190cf2409fbbbd9953f8b7a3687e64daa86f
wasantha2000 <www.wasanthakeerthi9@gmail.com>
2025-04-23 23:52:08 +0530

    Update LecStudentAttendance

--- system2/src/LecStudentAttendance.java
+++ system2/src/LecStudentAttendance.java
@@ -1,2 +1,22 @@
+import javax.swing.*;
+
 public class LecStudentAttendance {
+    private JPanel panel1;
+    private JTextField textField1;
+    private JButton viewButton;
+    private JButton viewButton1;
+    private JButton backButton;
+    private JButton clearButton;
+    private JTable table1;
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("LecStudentAttendance");
+        frame.setContentPane(new LecStudentAttendance().panel1);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+        frame.setLocationRelativeTo(null);
+        frame.setResizable(false);
+        frame.setSize(1000,500);
+    }
 }
