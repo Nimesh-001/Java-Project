a3f2dd07103e0486ffcdeb8a9c1452229b314a09
wasantha2000 <www.wasanthakeerthi9@gmail.com>
2025-04-25 00:27:48 +0530

    Update Lecturdashbord.java

--- system2/src/Lecturdashbord.java
+++ system2/src/Lecturdashbord.java
@@ -1,4 +1,6 @@
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class Lecturdashbord {
     private JPanel panel1;
@@ -11,18 +13,62 @@ public class Lecturdashbord {
     private JButton marksButton1;
     private JButton studentsButton1;
     private JButton noticeButton1;
+    private JButton editProfileButton;
     private JButton button10;
+    private JTextField textField1;
 
 
-    public static void main(String[] args) {
+
+    public Lecturdashbord() {
+
         JFrame frame = new JFrame("Lecturdashbord");
-        frame.setContentPane(new Lecturdashbord().panel1);
+        frame.setContentPane(this.panel1);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.pack();
         frame.setVisible(true);
         frame.setSize(1000,500);
         frame.setResizable(false);
         frame.setLocationRelativeTo(null);
+
+        coursesButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                frame.dispose();
+                new LecCourses();
+            }
+        });
+        editProfileButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                frame.dispose();
+                new LecEditProfile();
+            }
+        });
+        marksButton1.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                frame.dispose();
+                new LecMarks();
+            }
+        });
+        studentsButton1.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                frame.dispose();
+                new LecStudentdashbord();
+            }
+        });
+        noticeButton1.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                frame.dispose();
+                new LecNotice();
+            }
+        });
+    }
+
+    public static void main(String[] args) {
+        new Lecturdashbord();
 
     }
 }
