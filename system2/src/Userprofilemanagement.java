d3af1a176ac5dcf8f3e82c0e53a0d0a28685c9e0
Nimesh Sanka Lakmal <nimeshsanka1209@gmail.com>
2025-04-25 12:39:00 +0530

    Update Userprofilemanagement.java

--- system2/src/Userprofilemanagement.java
+++ system2/src/Userprofilemanagement.java
@@ -51,6 +51,8 @@ public class Userprofilemanagement {
         backButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
+                frame.dispose();
+                new Admindashbord();
 
             }
         });
