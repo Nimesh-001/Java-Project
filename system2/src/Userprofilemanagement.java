30bfbae1a4592e9ae788b1a08f41b767b2c24810
Nimesh Sanka Lakmal <nimeshsanka1209@gmail.com>
2025-04-25 12:35:09 +0530

    update

--- system2/src/Userprofilemanagement.java
+++ system2/src/Userprofilemanagement.java
@@ -1,2 +1,69 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 public class Userprofilemanagement {
+    private JPanel panel1;
+    private JButton adminButton;
+    private JButton lecturerButton;
+    private JButton studentButton;
+    private JButton tOButton;
+    private JButton backButton;
+    private JButton logoutButton;
+
+
+    public Userprofilemanagement() {
+
+        JFrame frame = new JFrame("Userprofilemanagement");
+        frame.setContentPane(this.panel1);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+        frame.setLocationRelativeTo(null);
+        frame.setResizable(false);
+        frame.setSize(1000,500);
+
+
+        adminButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+        lecturerButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+        studentButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+        tOButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+        backButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+        logoutButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+    }
+
+    public static void main(String[] args) {
+        new Userprofilemanagement();
+
+    }
 }
