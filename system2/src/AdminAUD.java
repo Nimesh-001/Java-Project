e78f2ec165b552f67278012f40c2952fdcb1b059
Nimesh Sanka Lakmal <nimeshsanka1209@gmail.com>
2025-04-25 13:08:07 +0530

    update

---
+++ system2/src/AdminAUD.java
@@ -0,0 +1,60 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class AdminAUD {
+    private JPanel panel1;
+    private JButton ADDButton;
+    private JButton UPDATEButton;
+    private JButton DELETEButton;
+    private JButton backButton;
+    private JButton logoutButton;
+
+    public AdminAUD() {
+
+        JFrame frame = new JFrame("AdminAUD");
+        frame.setContentPane(this.panel1);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+        frame.setLocationRelativeTo(null);
+        frame.setResizable(false);
+        frame.setSize(1000,500);
+
+        ADDButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+        UPDATEButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+        DELETEButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+        backButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+        logoutButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+    }
+
+    public static void main(String[] args) {
+        new AdminAUD();
+
+    }
+}
