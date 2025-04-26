553cbdd9931b1d520078b47260c1364636367ac7
Nimesh Sanka Lakmal <nimeshsanka1209@gmail.com>
2025-04-27 02:02:41 +0530

    update

--- system2/src/StudentViewTimetable.java
+++ system2/src/StudentViewTimetable.java
@@ -1,2 +1,56 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.sql.Connection;
+
 public class StudentViewTimetable {
+
+    Dbconnector dbc = new Dbconnector();
+    Connection con = dbc.getConnection();
+
+    private JPanel panel1;
+    private JButton backButton;
+    private JButton logoutButton;
+    private JButton VIEWButton;
+    private JTable table1;
+
+    public StudentViewTimetable() {
+
+        JFrame frame = new JFrame("StudentViewTimetable");
+        frame.setContentPane(this.panel1);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+        frame.setLocationRelativeTo(null);
+        frame.setResizable(false);
+        frame.setResizable(true);
+
+        backButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                frame.dispose();
+                //chavindya dashbord here.......
+
+            }
+        });
+        logoutButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                frame.dispose();
+                new form();
+
+            }
+        });
+        VIEWButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+    }
+
+    public static void main(String[] args) {
+        new StudentViewTimetable();
+
+    }
 }
